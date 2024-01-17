07ec14e772978a007711d5637b1b9de97d3f70a2
Levina Tani <edelaida@gmail.com>
2024-01-17 15:24:17 +0200

    modalImg

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-
-const inputElem = document.querySelector("#name-input");
-const outputElem = document.querySelector("#name-output");
-inputElem.addEventListener("input", (eventIn) => {
-    outputElem.textContent = eventIn.currentTarget.value.trim() || "Anonymous";     
-}); 
-
